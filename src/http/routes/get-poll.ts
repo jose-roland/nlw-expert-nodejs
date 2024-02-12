9dc8205db9c0bae3a74e1966db6cb4931611566f
José Roland <jose.sroland@gmail.com>
2024-02-12 15:54:16 -0400

    Score add score in get poll

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,6 +1,7 @@
 import z from "zod";
 import { prisma } from "../../lib/prisma";
 import { FastifyInstance } from "fastify";
+import { redis } from "../../lib/redis";
 
 export async function findPollById(app: FastifyInstance) {
   app.get("/poll/:pollId", async (req, res) => {
@@ -24,6 +25,34 @@ export async function findPollById(app: FastifyInstance) {
       },
     });
 
-    return res.send({ poll });
+    if (!poll) {
+      return res.status(400).send({ message: "Poll not found" });
+    }
+
+    const result = await redis.zrange(pollId, 0, -1, "WITHSCORES");
+
+    const votes = result.reduce((obj, pollOption, index) => {
+      if (index % 2 === 0) {
+        const score = result[index + 1];
+
+        Object.assign(obj, { [pollOption]: Number(score) });
+      }
+
+      return obj;
+    }, {} as Record<string, number>);
+
+    return res.send({
+      poll: {
+        id: poll.id,
+        title: poll.title,
+        option: poll.options.map((option) => {
+          return {
+            id: option.id,
+            title: option.title,
+            score: option.id in votes ? votes[option.id] : 0,
+          };
+        }),
+      },
+    });
   });
 }
